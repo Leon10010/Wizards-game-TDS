61e4d678d7c9258324499297b8985dee727a00fd
Leon10010 <35190374+Leon10010@users.noreply.github.com>
2018-01-07 11:50:52 +0100

    Create Handler.java

---
+++ Handler.java
@@ -0,0 +1,70 @@
+import java.util.LinkedList;
+import java.awt.Graphics;
+
+public class Handler {
+	
+	LinkedList<GameObjekt> object = new LinkedList<GameObjekt>();
+	
+	private boolean up = false, down = false, right = false, left = false;
+	
+	public void tick() {
+		for(int i = 0; i < object.size(); i++) {
+			GameObjekt tempObject = object.get(i);
+			
+			tempObject.tick();
+		}
+	}
+	
+	public void render(Graphics g) {
+		for(int i = 0; i < object.size(); i++) {
+			GameObjekt tempObject = object.get(i);
+			
+			tempObject.render(g);
+			
+		}
+		
+	}
+	
+	public void addObject(GameObjekt tempObject) {
+		object.add(tempObject);
+	}
+	
+	public void removeObject(GameObjekt tempObject) {
+		object.remove(tempObject);
+	}
+
+	public boolean isUp() {
+		return up;
+	}
+
+	public void setUp(boolean up) {
+		this.up = up;
+	}
+
+	public boolean isDown() {
+		return down;
+	}
+
+	public void setDown(boolean down) {
+		this.down = down;
+	}
+
+	public boolean isRight() {
+		return right;
+	}
+
+	public void setRight(boolean right) {
+		this.right = right;
+	}
+
+	public boolean isLeft() {
+		return left;
+	}
+
+	public void setLeft(boolean left) {
+		this.left = left;
+	}
+	
+	
+
+}
